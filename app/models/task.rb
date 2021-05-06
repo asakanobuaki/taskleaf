71480923e58d99fb6da8f88e2c471299058c67bb
asakanobuaki <s1531007@gmail.com>
2021-05-06 15:20:51 +0900

    add taskleaf

---
+++ app/models/task.rb
@@ -0,0 +1,16 @@
+class Task < ApplicationRecord
+  #validates :name, presence: true
+  #validates :name, length: { maximum: 30 }
+  validate :varidate_name_not_including_comma
+
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
+
+private
+
+def varidate_name_not_including_comma
+  errors.add(:name, 'にコンマを含めることはできません') if name&.include?(',')
+end
+
+end
