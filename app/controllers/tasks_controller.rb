21b40a5822bf9bd2f8dafc2bc86bd5f48353387e
asakanobuaki <s1531007@gmail.com>
2021-10-17 15:49:33 +0900

    Add: 新規投稿確認画面追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,10 +22,15 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
     
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
-      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+      redirect_to tasks_path, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
@@ -34,6 +39,11 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   private
